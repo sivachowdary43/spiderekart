99c725df5dbbe6f083d1238ddd4a64c86f20f790
sivachowdary43 <sivaprasad.g@spiderindia.com>
2020-09-29 16:00:44 +0530

    Updates

---
+++ precache-manifest.dcaaf6c7c664fcb826e25970efca7bf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c4e82d5f84a669d68163c9ceb225bd",
+    "url": "/sivachowdary43/spiderekart.git/index.html"
+  },
+  {
+    "revision": "7b560a561928c27cd9e0",
+    "url": "/sivachowdary43/spiderekart.git/static/css/2.34c0de18.chunk.css"
+  },
+  {
+    "revision": "b8af5c90d530ac201448",
+    "url": "/sivachowdary43/spiderekart.git/static/css/main.56dca798.chunk.css"
+  },
+  {
+    "revision": "7b560a561928c27cd9e0",
+    "url": "/sivachowdary43/spiderekart.git/static/js/2.9bb8e1aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sivachowdary43/spiderekart.git/static/js/2.9bb8e1aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8af5c90d530ac201448",
+    "url": "/sivachowdary43/spiderekart.git/static/js/main.3cded487.chunk.js"
+  },
+  {
+    "revision": "dea9de1ef7cc0597ae28",
+    "url": "/sivachowdary43/spiderekart.git/static/js/runtime-main.80e2becc.js"
+  }
+]);
